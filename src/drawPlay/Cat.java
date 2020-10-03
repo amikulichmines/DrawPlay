0dd54853594fad9753a847ac1af4aae48ef70b0a
amikulichmines <amikulich@mymail.mines.edu>
2020-10-02 18:43:07 -0600

    Made eyes unnaturally large

--- src/drawPlay/Cat.java
+++ src/drawPlay/Cat.java
@@ -12,8 +12,8 @@ public class Cat {
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 10;
+	private static final int EYE_HEIGHT = 60;
+	private static final int EYE_WIDTH = 40;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
@@ -35,7 +35,7 @@ public class Cat {
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-		x += EYE_SEPARATION +40 ;
+		x += EYE_SEPARATION +40;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
 		g2.setColor(Color.pink);
